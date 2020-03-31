464633f32308abcabb9f47dd4aeee318fdfea4f2
Manuel Goncalves <man281192@gmail.com>
2020-03-31 00:22:20 -0300

    Hello world!

---
+++ android/app/src/main/kotlin/manuel28g/com/MainActivity.kt
@@ -0,0 +1,12 @@
+package manuel28g.com
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
